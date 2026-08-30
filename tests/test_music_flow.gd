e97303ec6d4bc34fb25d04dc7520045b674c014c
YLMstring <61271096+YLMstring@users.noreply.github.com>
2026-08-31 00:24:03 +0800

    Stabilize music flow test teardown

--- tests/test_music_flow.gd
+++ tests/test_music_flow.gd
@@ -23,7 +23,7 @@ func _run() -> void:
 	await _test_testing_unlock_does_not_trigger_bixie()
 	_cleanup_path(NORMAL_SAVE_PATH)
 	_cleanup_path(TESTING_SAVE_PATH)
-	await process_frame
+	await create_timer(0.1).timeout
 	await process_frame
 	_finish()
 
